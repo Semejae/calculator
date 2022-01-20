03e2f03cf6be52696d1409aa54a829561fca824e
Anthony Gladden <a.gladden360@outlook.com>
2022-01-19 19:13:26 -0800

    add substract/multiply/remainder to scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -5,4 +5,15 @@ const number1 = parseInt(prompt('enter a number:'));
 
 const number2 = parseInt(prompt('enter another number:'));
 
-alert(add(number1,number2));
+const result = add(number1, number2);
+
+function subtract(number1, number2) {
+return number1 - number2;
+}
+function multiply(number1, number2) {
+return number1 * number2;
+}
+function remainder(number1, number2)  {
+return number1 / number2;
+}
+alert(result);
