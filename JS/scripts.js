04cd6c73dd79aae6d253792510656f4e088d1474
Anthony Gladden <a.gladden360@outlook.com>
2022-02-14 18:18:40 -0800

    Co-authored-by: Anthony Gladden <a.gladden360@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,12 +1,7 @@
+// business logic//
 function add(number1, number2) {        
 return number1 + number2;							
 }
-const number1 = parseInt(prompt('enter a number:'));
-
-const number2 = parseInt(prompt('enter another number:'));
-
-const result = add(number1, number2);
-
 function subtract(number1, number2) {
 return number1 - number2;
 }
@@ -16,4 +11,24 @@ return number1 * number2;
 function remainder(number1, number2)  {
 return number1 / number2;
 }
-alert(result);
+// user logic//
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
